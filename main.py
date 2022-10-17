fac3fc92689c4c1b7c90ea3b3590655514ca6191
yura555 <2001spku@gmail.com>
2022-10-17 21:18:37 +0300

    rang facts

--- main.py
+++ main.py
@@ -84,13 +84,7 @@ def generate_random_rules(code_max, n_max, n_generate, log_oper_choice=["and", "
         }
         rules.append(rule)
     shuffle(rules)
-    return (rules)
-
-
-def generate_seq_facts(M):
-    facts = list(range(0, M))
-    shuffle(facts)
-    return facts
+    return rules
 
 
 def generate_rand_facts(code_max, M):
@@ -100,60 +94,5 @@ def generate_rand_facts(code_max, M):
     return facts
 
 
-def parse_rules(rules):
-    result = []
-    for rule in rules:
-        oper = ''.join(rule['if'].keys())
-        result.append([oper, set(rule['if'][oper]), rule['then']])
-    result.sort()
-    return result
 
 
-def check_validate_rules(rules):
-    rules = parse_rules(rules)
-    for i in range(0, len(rules) - 1):
-        for j in range(i + 1, len(rules)):
-            if rules[i][0] == rules[j][0] and rules[i][1] == rules[j][1] and rules[i][2] != rules[j][2]:
-                rules[i][2] = rules[j][2] = -1
-
-    return rules
-
-
-def check_facts(rules, facts):
-    set_facts = set(facts)
-    result = []
-    for rule in rules:
-        if rule[0] == 'or':
-            result.append(rule[2] if any([z in set_facts for z in rule[1]]) else 0)
-        if rule[0] == 'and':
-            result.append(rule[2] if all([z in set_facts for z in rule[1]]) else 0)
-        if rule[0] == 'not':
-            result.append(rule[2] if not any([z in set_facts for z in rule[1]]) else 0)
-
-    return result
-
-
-# samples:
-#a = generate_simple_rules(100, 4, 10)
-#print(generate_simple_rules(100, 4, 10))
-#print(generate_random_rules(100, 4, 10))
-#print(generate_stairway_rules(100, 4, 10, ["or"]))
-#print(generate_ring_rules(100, 4, 10, ["or"]))
-
-# generate rules and facts and check time
-time_start = time()
-N = 100000
-M = 1000
-rules = generate_simple_rules(100, 4, N)
-facts = generate_rand_facts(100, M)
-
-
-rules = check_validate_rules(rules)
-print(f"{N} rules validated {round(time() - time_start, 6)} seconds")
-
-# check facts vs rules
-time_start = time()
-
-# YOUR CODE HERE
-check_facts(rules, facts)
-print("%d facts validated vs %d rules in %f seconds" % (M, N, time() - time_start))
