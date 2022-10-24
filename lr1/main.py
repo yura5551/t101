fac799671032a7f7e2f397864490e2da09c8646b
yura5551 <2001spku@gmail.com>
2022-10-24 18:40:01 +0300

    start

---
+++ lr1/main.py
@@ -0,0 +1,219 @@
+from random import choice, shuffle, randint
+from time import time
+
+
+def generate_simple_rules(code_max, n_max, n_generate, log_oper_choice=["and", "or", "not"]):
+    rules = []
+    for j in range(0, n_generate):
+
+        log_oper = choice(log_oper_choice)
+        if n_max < 2:
+            n_max = 2
+        n_items = randint(2, n_max)
+        items = []
+        for i in range(0, n_items):
+            items.append(randint(1, code_max))
+        rule = {
+            'if': {
+                log_oper: items
+            },
+            'then': code_max + j
+        }
+        rules.append(rule)
+    shuffle(rules)
+    return (rules)
+
+
+def generate_stairway_rules(code_max, n_max, n_generate, log_oper_choice=["and", "or", "not"]):
+    rules = []
+    for j in range(0, n_generate):
+        log_oper = choice(log_oper_choice)
+        if n_max < 2:
+            n_max = 2
+        n_items = randint(2, n_max)
+        items = []
+        for i in range(0, n_items):
+            items.append(i + j)
+        rule = {
+            'if': {
+                log_oper: items
+            },
+            'then': i + j + 1
+        }
+        rules.append(rule)
+    shuffle(rules)
+    return rules
+
+
+def generate_ring_rules(code_max, n_max, n_generate, log_oper_choice=["and", "or", "not"]):
+    rules = generate_stairway_rules(code_max, n_max, n_generate - 1, log_oper_choice)
+    log_oper = choice(log_oper_choice)
+    if n_max < 2:
+        n_max = 2
+    n_items = randint(2, n_max)
+    items = []
+    for i in range(0, n_items):
+        items.append(code_max - i)
+    rule = {
+        'if': {
+            log_oper: items
+        },
+        'then': 0
+    }
+    rules.append(rule)
+    shuffle(rules)
+    return (rules)
+
+
+def generate_random_rules(code_max, n_max, n_generate, log_oper_choice=["and", "or", "not"]):
+    rules = []
+    for j in range(0, n_generate):
+
+        log_oper = choice(log_oper_choice)  # not means and-not (neither)
+        if n_max < 2:
+            n_max = 2
+        n_items = randint(2, n_max)
+        items = []
+        for i in range(0, n_items):
+            items.append(randint(1, code_max))
+        rule = {
+            'if': {
+                log_oper: items
+            },
+            'then': randint(1, code_max)
+        }
+        rules.append(rule)
+    shuffle(rules)
+    return rules
+
+
+def generate_rand_facts(code_max, M):
+    facts = []
+    for i in range(0, M):
+        facts.append(randint(0, code_max))
+    return facts
+
+
+def proofs_check(rules_list, facts) -> []:
+    s_list = [[], [], []]
+    result = []
+    temp_it = 0
+    for rule in rules_list:
+        if rule != {}:
+            for key in rule['if'].keys():
+                if key == 'or':
+                    s_list[0].append(rule)
+                if key == 'and':
+                    s_list[1].append(rule)
+                if key == 'not':
+                    s_list[2].append(rule)
+    for rule in s_list[0]:  # or
+        for item in rule['if']['or']:
+            size = len(rule['if']['or'])
+            if item in facts:
+                result.append(rule['then'])
+                temp_it = 0
+                break
+            else:
+                temp_it += 1
+                if temp_it == size:
+                    result.append(0)
+                    temp_it = 0
+    for rule in s_list[1]:  # and
+        for item in rule['if']['and']:
+            size = len(rule['if']['and'])
+            if item in facts:
+                temp_it += 1
+        if temp_it == size:
+            result.append(rule['then'])
+            temp_it = 0
+        else:
+            result.append(0)
+            temp_it = 0
+
+    for rule in s_list[2]:  # not
+        for item in rule['if']['not']:
+            size = len(rule['if']['not'])
+            if item not in facts:
+                temp_it += 1
+        if temp_it == size:
+            result.append(rule['then'])
+            temp_it = 0
+        else:
+            result.append(0)
+            temp_it = 0
+    return result
+
+
+def validate_rules(rules_list):
+    parse_rules = [[], [], []]
+    for rule in rules_list:
+        if rule['if']:
+            parse_rules[0].append(rule['if'])
+        if rule['then']:
+            parse_rules[1].append(rule['then'])
+    for i in range(len(rules_list) - 1):
+        for j in range(i + 1, len(rules_list) - 1):
+            if i >= j:
+                return 0
+            if parse_rules[1][i] == parse_rules[1][j]:
+                if ('and' in rules_list[i].keys() and 'not' in rules_list[j].keys) or (
+                        'and' in rules_list[j].keys() and 'not' in rules_list[i].keys):
+                    if parse_rules[0][j]['and'] == parse_rules[0][i]['not'] or parse_rules[0][i]['and'] == \
+                            parse_rules[0][j]['not']:
+                        rules_list[j].clear()
+                        rules_list[i].clear()
+                if ('or' in rules_list[j].keys() and 'not' in rules_list[i].keys) or (
+                        'or' in rules_list[i].keys() and 'not' in rules_list[j].keys):
+                    if parse_rules[0][j]['or'] == parse_rules[0][i]['not'] or parse_rules[0][i]['or'] == \
+                            parse_rules[0][j]['not']:
+                        rules_list[j].clear()
+                        rules_list[i].clear()
+            if 'not' in parse_rules[0][i].keys() and 'not' in parse_rules[0][j].keys():
+                if parse_rules[1][i] in parse_rules[0][j]['not'] and parse_rules[1][j] in \
+                        parse_rules[0][i]['not']:
+                    rules_list[i].clear()
+                    rules_list[j].clear()
+                if parse_rules[1][i] in parse_rules[0][j]['not'] and parse_rules[1][j] not in \
+                        parse_rules[0][i]['not']:
+                    rules_list[i].clear()
+                    rules_list[j].clear()
+    for rule in rules_list:
+        if rule != {}:
+            parse_rules[2].append(rule)
+    return parse_rules[2]
+
+
+def main():
+    number_of_rules = 10000
+    number_of_facts = 1000
+    rules = generate_simple_rules(100, 4, number_of_rules)
+    random_rules = generate_random_rules(100, 4, number_of_rules)
+    stairway_rules = generate_stairway_rules(100, 4, number_of_rules)
+    ring_rules = generate_ring_rules(100, 4, number_of_rules)
+
+    facts = generate_rand_facts(number_of_facts, number_of_rules)
+    unique_facts = set(facts)
+
+    time_start = time()
+    correct_simple = validate_rules(rules)
+    correct_random = validate_rules(random_rules)
+    correct_stairway = validate_rules(stairway_rules)
+    correct_ring = validate_rules(ring_rules)
+    validate_time = time() - time_start
+    print(number_of_rules, "rules validate in seconds:", validate_time)
+
+    # facts vs rules
+    time_start = time()
+    proofs_check(correct_simple, unique_facts)
+    proofs_check(correct_random, unique_facts)
+    proofs_check(correct_stairway, unique_facts)
+    proofs_check(correct_ring, unique_facts)
+    check_time = time() - time_start
+    print(number_of_rules, "rules check in seconds:", check_time)
+
+    print("Finally time:",  (validate_time + check_time))
+
+
+if __name__ == "__main__":
+    main()
---
+++ lr2/generator.py
@@ -0,0 +1,178 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from time import time
+
+
+# generates x and y numpy arrays for
+# y = a*x + b + a * noise
+# in range -1 .. 1
+# with random noise of given amplitude (noise)
+# vizualizes it and unloads to csv
+def generate_linear(a, b, noise, filename, size=100):
+    print('Generating random data y = a*x + b')
+    x = 2 * np.random.rand(size, 1) - 1
+    y = a * x + b + noise * a * (np.random.rand(size, 1) - 0.5)
+    data = np.hstack((x, y))
+    np.savetxt(filename, data, delimiter=',')
+    return (x, y)
+
+
+# thats an example of linear regression using polyfit
+def linear_regression_numpy(filename):
+    # now let's read it back
+    with open(filename, 'r') as f:
+        data = np.loadtxt(f, delimiter=',')
+    # split to initial arrays
+    x, y = np.hsplit(data, 2)
+    # printing shapes is useful for debugging
+    print(np.shape(x))
+    print(np.shape(y))
+    # our model
+    time_start = time()
+    model = np.polyfit(np.transpose(x)[0], np.transpose(y)[0], 1)
+    time_end = time()
+    print(f"polyfit in {time_end - time_start} seconds")
+    # our hypothesis for give x
+    h = model[0] * x + model[1]
+
+    # and check if it's ok
+    plt.title("Linear regression task")
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.plot(x, y, "b.", label='experiment')
+    plt.plot(x, h, "r", label='model')
+    plt.legend()
+    plt.show()
+    return model
+
+
+def linear_regression_exact(filename):
+    print("Ex1: your code here - exact solution usin invert matrix")
+    return
+
+
+def check(model, ground_truth):
+    if len(model) != len(ground_truth):
+        print("Model is inconsistent")
+        return False
+    else:
+        r = np.dot(model - ground_truth, model - ground_truth) / (np.dot(ground_truth, ground_truth))
+        print(r)
+        if r < 0.0001:
+            return True
+        else:
+            return False
+
+
+# Ex1: make the same with polynoms
+
+# generates x and y numpy arrays for
+# y = a_n*X^n + ... + a2*x^2 + a1*x + a0 + noise
+# in range -1 .. 1
+# with random noise of given amplitude (noise)
+# vizualizes it and unloads to csv
+def generate_poly(a, n, noise, filename, size=100):
+    x = 2 * np.random.rand(size, 1) - 1
+    y = np.zeros((size, 1))
+    print(np.shape(x))
+    print(np.shape(y))
+    if len(a) != (n + 1):
+        print(f'ERROR: Length of polynomial coefficients ({len(a)}) must be the same as polynomial degree {n}')
+        return
+    for i in range(0, n + 1):
+        y = y + a[i] * np.power(x, i) + noise * (np.random.rand(size, 1) - 0.5)
+    print(np.shape(x))
+    data = np.hstack((x, y))
+    np.savetxt(filename, data, delimiter=',')
+
+
+def polynomial_regression_numpy(filename):
+    print("Ex1: your code here")
+    time_start = time()
+    print("Ex1: your code here")
+    time_end = time()
+    print(f"polyfit in {time_end - time_start} seconds")
+
+
+# Ex.2 gradient descent for linear regression without regularization
+
+# find minimum of function J(theta) using gradient descent
+# alpha - speed of descend
+# theta - vector of arguments, we're looking for the optimal ones (shape is 1 х N)
+# J(theta) function which is being minimizing over theta (shape is 1 x 1 - scalar)
+# dJ(theta) - gradient, i.e. partial derivatives of J over theta - dJ/dtheta_i (shape is 1 x N - the same as theta)
+# x and y are both vectors
+
+def gradient_descent_step(dJ, theta, alpha):
+    print("your code goes here")
+
+    return theta
+
+
+# get gradient over all xy dataset - gradient descent
+def get_dJ(x, y, theta):
+    theta_new = theta
+    print("your code goes here - calculate new theta")
+    return theta_new
+
+
+# get gradient over all minibatch of size M of xy dataset - minibatch gradient descent
+def get_dJ_minibatch(x, y, theta, M):
+    theta_new = theta
+    print("your code goes here - calculate new theta")
+    return theta_new
+
+
+# get gradient over all minibatch of single sample from xy dataset - stochastic gradient descent
+def get_dJ_sgd(x, y, theta):
+    theta_new = theta
+    print("your code goes here - calculate new theta")
+    return theta_new
+
+
+# try each of gradient decsent (complete, minibatch, sgd) for varius alphas
+# L - number of iterations
+# plot results as J(i)
+def minimize(theta, x, y, L):
+    # n - number of samples in learning subset, m - ...
+    n = 12345  # <-- calculate it properly!
+    theta = np.zeros(n)  # you can try random initialization
+    dJ = np.zeros(n)
+    for i in range(0, L):
+        theta = get_dJ(x, y, theta)  # here you should try different gradient descents
+        J = 0  # here you should calculate it properly
+    # and plot J(i)
+    print("your code goes here")
+    return
+
+
+if __name__ == "__main__":
+    generate_linear(1, -3, 1, 'linear.csv', 100)
+    model = linear_regression_numpy("linear.csv")
+    print(f"Is model correct?\n{check(model, np.array([1, -3]))}")
+    # ex1 . - exact solution
+    # model_exact = linear_regression_exact("linear.csv")
+    # check(model_exact, np.array([-3,1]))
+
+    # ex1. polynomial with numpy
+    generate_poly([1, 2, 3], 2, 0.5, 'polynomial.csv')
+    polynomial_regression_numpy("polynomial.csv")
+
+    # ex2. find minimum with gradient descent
+    # 0. generate date with function above
+    # 1. shuffle data into train - test - valid
+    # 2. call minuimize(...) and plot J(i)
+    # 3. call check(theta1, theta2) to check results for optimal theta
+
+    # ex3. polinomial regression
+    # 0. generate date with function generate_poly for degree=3, use size = 10, 20, 30, ... 100
+    # for each size:
+    # 1. shuffle data into train - test - valid
+    # Now we're going to try different degrees of model to aproximate our data, set degree=1 (linear regression)
+    # 2. call minimize(...) and plot J(i)
+    # 3. call check(theta1, theta2) to check results for optimal theta
+    # 4. plot min(J_train), min(J_test) vs size: is it overfit or underfit?
+    #
+    # repeat 0-4 for degres = 2,3,4
+
+    # ex3* the same with regularization
